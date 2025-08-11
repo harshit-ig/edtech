69803cfbed71a87916bb81369515674f918dc042
harshit-ig <sharshit0369@gmail.com>
2025-08-12 00:58:42 +0530

    demo frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {host: true}
+  server: {host: true,
+    allowedHosts: ['edtech-b3xr.onrender.com', 'localhost', '127.0.0.1']
+  }
 })
