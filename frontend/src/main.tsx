0b9d1dd4e54151bd7c86be537be0c17c9f668b3d
harshit-ig <sharshit0369@gmail.com>
2025-08-12 13:44:36 +0530

    updates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ContactPage from './pages/Contact.tsx'
-import HomePage from './pages/Home.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
+  { path: '/', element: <App /> },
   { path: '/contact', element: <ContactPage /> },
-  { path: '/home', element: <App /> },
 ])
 
 createRoot(document.getElementById('root')!).render(
