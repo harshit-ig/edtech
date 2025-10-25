8641d314708e7e669066515fcfba50741e61960c
harshit-ig <sharshit0369@gmail.com>
2025-10-25 17:54:11 +0530

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// vite.config.ts
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/.well-known/*',
+          dest: '.well-known'
+        }
+      ]
+    })],
   build: {
     rollupOptions: {
       output: {
